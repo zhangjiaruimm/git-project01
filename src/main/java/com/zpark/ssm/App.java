8995d1eaedc53d495bd88378fd6e2a54f20cd5ce
zjr <zjr2270668383@126.com>
2023-05-05 18:14:48 +0800

    second commit

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -3,6 +3,6 @@ package com.zpark.ssm;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git");
-        //System.out.println("Hello Git zjr master second commit!!");
+        System.out.println("Hello Git zjr master second commit!!!");
     }
 }
