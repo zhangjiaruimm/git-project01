eeac933917263a65539775dd72c2cfe3e0bc667f
zjr <zjr2270668383@126.com>
2023-05-06 09:40:42 +0800

    hotfix first modify

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -4,6 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello Git zjr master second commit!!!");
-        System.out.println("Hello Git zjr master first commit!!!");
+        System.out.println("Hello Git zjr hotfix first commit!!!");
     }
 }
